ac3a8490335e2a55af38311264d7115b5eac8eb6
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-18 15:53:11 +0700

    Integrate Sequelize with Express

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+const app = express()
+const port = 8000
+
+const models = require("./models")
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+
+app.get("/", (req, res) => {
+  res.send({
+    data: ""
+  })
+})
+
+app.get("/users", (req, res) => {
+  res.send({
+    data: ""
+  })
+})
+
+models.sequelize.sync().then(function() {
+  app.listen(port, err => {
+    console.log(`Server running at http://localhost:${port}`)
+  })
+})
